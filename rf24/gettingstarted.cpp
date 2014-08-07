db6f10ab147da9caac159389a60e71729c26d5dd
David Szotten <davidszotten@gmail.com>
2014-08-07 16:50:29 +0000

    only listen

--- rf24/gettingstarted.cpp
+++ rf24/gettingstarted.cpp
@@ -52,9 +52,6 @@ int main(int argc, char** argv){
   bool role_ping_out = true, role_pong_back = false;
   bool role = role_pong_back;
 
-  // Print preamble:
-  printf("RF24/examples/pingtest/\n");
-
   // Setup and configure rf radio
   radio.begin();
 
@@ -64,122 +61,48 @@ int main(int argc, char** argv){
   radio.printDetails();
 
 
-/********* Role chooser ***********/
-
-  printf("\n ************ Role Setup ***********\n");
-  string input = "";
-  char myChar = {0};
-  cout << "Choose a role: Enter 0 for pong_back, 1 for ping_out (CTRL+C to exit) \n>";
-  getline(cin,input);
-
-  if(input.length() == 1) {
-	myChar = input[0];
-	if(myChar == '0'){
-		cout << "Role: Pong Back, awaiting transmission " << endl << endl;
-	}else{  cout << "Role: Ping Out, starting transmission " << endl << endl;
-		role = role_ping_out;
-	}
-  }
 /***********************************/
   // This simple sketch opens two pipes for these two nodes to communicate
   // back and forth.
 
-    if ( role == role_ping_out )    {
-      radio.openWritingPipe(pipes[0]);
-      radio.openReadingPipe(1,pipes[1]);
-    } else {
-      radio.openWritingPipe(pipes[1]);
-      radio.openReadingPipe(1,pipes[0]);
-      radio.startListening();
-
-    }
+  radio.openWritingPipe(pipes[1]);
+  radio.openReadingPipe(1,pipes[0]);
+  radio.startListening();
 
 	// forever loop
 	while (1)
 	{
-		if (role == role_ping_out)
-		{
-			// First, stop listening so we can talk.
-			radio.stopListening();
-
-			// Take the time, and send it.  This will block until complete
-
-			printf("Now sending...\n");
-			unsigned long time = millis();
-
-			bool ok = radio.write( &time, sizeof(unsigned long) );
-
-			if (!ok){
-				printf("failed.\n");
-			}
-			// Now, continue listening
-			radio.startListening();
-
-			// Wait here until we get a response, or timeout (250ms)
-			unsigned long started_waiting_at = millis();
-			bool timeout = false;
-			while ( ! radio.available() && ! timeout ) {
-				if (millis() - started_waiting_at > 200 )
-					timeout = true;
-			}
-
-
-			// Describe the results
-			if ( timeout )
-			{
-				printf("Failed, response timed out.\n");
-			}
-			else
-			{
-				// Grab the response, compare, and send to debugging spew
-				unsigned long got_time;
-				radio.read( &got_time, sizeof(unsigned long) );
-
-				// Spew it
-				printf("Got response %lu, round-trip delay: %lu\n",got_time,millis()-got_time);
-			}
-
-			// Try again 1s later
-			// delay(1000);
-
-			sleep(1);
-
-		}
-
 		//
 		// Pong back role.  Receive each packet, dump it out, and send it back
 		//
 
-		if ( role == role_pong_back )
-		{
 
-			// if there is data ready
-			//printf("Check available...\n");
+        // if there is data ready
+        //printf("Check available...\n");
 
-			if ( radio.available() )
-			{
-				// Dump the payloads until we've gotten everything
-				unsigned long got_time;
+        if ( radio.available() )
+        {
+            // Dump the payloads until we've gotten everything
+            unsigned long got_time;
 
 
-				// Fetch the payload, and see if this was the last one.
-				radio.read( &got_time, sizeof(unsigned long) );
+            // Fetch the payload, and see if this was the last one.
+            radio.read( &got_time, sizeof(unsigned long) );
 
-				radio.stopListening();
+            radio.stopListening();
 
-				radio.write( &got_time, sizeof(unsigned long) );
+            radio.write( &got_time, sizeof(unsigned long) );
 
-				// Now, resume listening so we catch the next packets.
-				radio.startListening();
+            // Now, resume listening so we catch the next packets.
+            radio.startListening();
 
-				// Spew it
-				printf("Got payload(%d) %lu...\n",sizeof(unsigned long), got_time);
+            // Spew it
+            printf("Got payload(%d) %lu...\n",sizeof(unsigned long), got_time);
 
-				delay(925); //Delay after payload responded to, minimize RPi CPU time
+            delay(925); //Delay after payload responded to, minimize RPi CPU time
 
-			}
+        }
 
-		}
 
 	} // forever loop
 
