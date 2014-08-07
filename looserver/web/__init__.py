d15eeecf70722e1cf7fd41f6dfe0533e668eaeed
David Szotten <davidszotten@gmail.com>
2014-08-07 16:00:06 +0100

    initial web component

---
+++ looserver/web/__init__.py
@@ -0,0 +1,100 @@
+from gevent import monkey
+monkey.patch_all()
+
+import time
+from threading import Thread
+from flask import Flask, render_template, session, request
+from flask.ext.socketio import SocketIO, emit, join_room, leave_room
+
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+thread = None
+
+
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
+    while True:
+        count += 1
+        socketio.emit(
+            'update', {
+                'loo': {
+                    'identifier': 'foo',
+                    'in_use': count % 2 == 0,
+                },
+            },
+            namespace='/loos')
+        time.sleep(1)
+
+
+@app.route('/')
+def index():
+    global thread
+    if thread is None:
+        thread = Thread(target=background_thread)
+        thread.start()
+    return render_template('index.html', loos={
+        'foo': {
+            'identifier': 'foo',
+            'label': 'my foo',
+            'floor': 1,
+            'in_use': False,
+        },
+    })
+
+
+@socketio.on('my event', namespace='/loos')
+def test_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my response',
+         {'data': message['data'], 'count': session['receive_count']})
+
+
+@socketio.on('my broadcast event', namespace='/test')
+def test_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my response',
+         {'data': message['data'], 'count': session['receive_count']},
+         broadcast=True)
+
+
+@socketio.on('join', namespace='/test')
+def join(message):
+    join_room(message['room'])
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my response',
+         {'data': 'In rooms: ' + ', '.join(request.namespace.rooms),
+          'count': session['receive_count']})
+
+
+@socketio.on('leave', namespace='/test')
+def leave(message):
+    leave_room(message['room'])
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my response',
+         {'data': 'In rooms: ' + ', '.join(request.namespace.rooms),
+          'count': session['receive_count']})
+
+
+@socketio.on('my room event', namespace='/test')
+def send_room_message(message):
+    session['receive_count'] = session.get('receive_count', 0) + 1
+    emit('my response',
+         {'data': message['data'], 'count': session['receive_count']},
+         room=message['room'])
+
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    emit('my response', {'data': 'Connected', 'count': 0})
+
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
+
+
+if __name__ == '__main__':
+    socketio.run(app)
---
+++ looserver/web/static/js/src/app.js
@@ -0,0 +1,65 @@
+/** @jsx React.DOM */
+
+var App = React.createClass({
+    getInitialState: function() {
+        return {
+            'loos': this.props.loos
+        }
+    },
+    componentDidMount: function() {
+        var socket = io.connect(
+            'http://' + document.domain + ':' + location.port + '/loos'
+        );
+        socket.on('update', function(data) {
+            var loo = data.loo;
+            var identifier = loo.identifier;
+            var inUse = loo.in_use;
+
+            var updateAction = {};
+            updateAction[identifier] = {'in_use': {$set: inUse}}
+
+            var updatedLoos = React.addons.update(
+                this.state.loos, updateAction);
+            this.setState({loos: updatedLoos});
+        }.bind(this));
+    },
+    render: function() {
+        var loos = _.map(this.state.loos, function(loo) {
+            return <Loo loo={ loo }/>
+        });
+        return (
+            <div className="row">
+                <h2>Hello world</h2>
+                { loos }
+            </div>
+        )
+    }
+});
+
+
+var Loo = React.createClass({
+    render: function() {
+        var cx = React.addons.classSet;
+        var classes = cx({
+            "columns": true,
+            "large-6": true,
+            "loo": true,
+            "loo-unavailable": this.props.loo.in_use
+        });
+        var statusString = function(in_use) {
+            if (in_use) {
+                return "Engaged";
+            }
+            else {
+                return "Available";
+            }
+        };
+        return (
+            <div className={classes}>
+                <h2>{ this.props.loo.label }</h2>
+                <h3>{ statusString(this.props.loo.in_use) }</h3>
+
+            </div>
+        )
+    }
+});
